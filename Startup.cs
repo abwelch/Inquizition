920974e485723c94512636807c3a1720639d4474
abwelch <abw28@zips.uakron.edu>
2020-01-07 20:05:32 -0600

    small css/layout changes

--- Startup.cs
+++ Startup.cs
@@ -65,7 +65,7 @@ namespace Inquizition
             {
                 // Cookie settings
                 options.Cookie.HttpOnly = true;
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(15);
 
                 options.LoginPath = "/Identity/Account/Login";
                 options.AccessDeniedPath = "/Identity/Account/AccessDenied";
