ebc217ea8e867583ac063e107d960aacb1c36169
abwelch <abw28@zips.uakron.edu>
2020-01-10 02:19:19 -0600

    Create overview user info model

---
+++ Models/UserInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inquizition.Models
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public bool IntroCompleted { get; set; }
+        [Required]
+        public bool EmailConfirmed { get; set; }
+        [Required]
+        public int TotalFriends { get; set; }
+        [Required]
+        public int TotalSets { get; set; }
+        [Required]
+        public int TotalBookmarks { get; set; }
+
+        // Add foreign keys for study set tables 
+
+    }
+}
