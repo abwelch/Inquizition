6945ca0116dba05772b518818c1a066bd7132cf2
abwelch <abw28@zips.uakron.edu>
2020-01-10 02:12:15 -0600

    Implement authentication and recording of user in table

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,21 +6,50 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Inquizition.Models;
+using Inquizition.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace Inquizition.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly InquizitionContext _dbContext;
+        private readonly UserManager<IdentityUser> _userManager;
+        private UserInfo AuthenticatedUser { get; set; }
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            InquizitionContext context,
+            UserManager<IdentityUser> userManager)
         {
             _logger = logger;
+            _dbContext = context;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
         {
-            // Add code to authenticate user and display username if authenticated
+            if(User.Identity.IsAuthenticated)
+            {
+                // Retrieve user info from table
+                AuthenticatedUser = _dbContext.UserOverviewInfo.Where(u => u.Username == User.Identity.Name).SingleOrDefault();
+                if (AuthenticatedUser == null)
+                {
+                    AuthenticatedUser = new UserInfo
+                    {
+                        // Add entry to table
+                        Username = User.Identity.Name,
+                        IntroCompleted = false,
+                        EmailConfirmed = _userManager.IsEmailConfirmedAsync(_userManager.GetUserAsync(User).Result).Result,
+                        TotalFriends = 0,
+                        TotalSets = 0,
+                        TotalBookmarks = 0
+                    };
+                    _dbContext.UserOverviewInfo.Add(AuthenticatedUser);
+                    _dbContext.SaveChanges();
+                }
+                return View(AuthenticatedUser);              
+            }
             return View();
         }
 
