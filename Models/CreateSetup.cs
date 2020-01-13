2dee8e49a228f7d006a402a6c595e86df9adfa30
abwelch <abw28@zips.uakron.edu>
2020-01-12 21:23:41 -0600

    Model for initial setup inputs

---
+++ Models/CreateSetup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inquizition.Models
+{
+    public class CreateSetup
+    {
+        public string InquizitionName { get; set; }
+
+        public string SelectedAssessment { get; set; }
+
+        public bool IsPrivate { get; set; }
+    }
+}
