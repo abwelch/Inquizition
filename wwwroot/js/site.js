df511f699bfba0f717712effdd73fd486810f4f9
abwelch <abw28@zips.uakron.edu>
2020-01-15 00:13:10 -0600

    Add functions for flashcard creation

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function ClearInputCard() {
+    document.getElementById("front-input").value = "";
+    document.getElementById("back-input").value = "";
+}
+
+function SetColor() {
+    var color = document.querySelector('input[name="CardColor"]:checked').value;
+    document.getElementById("body").className = "card-body " + color;
+    document.getElementById("answer").className = "card-body " + color;
+}
+
+function FlipCurrent() {
+    document.getElementById("whole-card").classList.toggle("flip-counter");
+}
+
+function FlipCreated(cardID) {
+    document.getElementById(cardID).classList.toggle("flip-counter");
+}
