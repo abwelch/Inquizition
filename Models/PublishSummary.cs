6df6d0bd7ffc90ddb410408645ed6c0065ce3422
abwelch <abw28@zips.uakron.edu>
2020-01-16 01:37:35 -0600

    Create view file for summary of published inquiz

---
+++ Models/PublishSummary.cs
@@ -0,0 +1,10 @@
+﻿namespace Inquizition.Models
+{
+    public class PublishSummary
+    {
+        public string Title { get; set; }
+        public string AssessmentType { get; set; }
+        public int TotalEntries { get; set; }
+        public bool Authenticated { get; set; }
+    }
+}
