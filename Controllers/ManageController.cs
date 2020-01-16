db5a6ff34ef31771962bcb1c120aae4d1f2382e4
abwelch <abw28@zips.uakron.edu>
2020-01-16 02:15:39 -0600

    Implement manage redirect

--- Controllers/ManageController.cs
+++ Controllers/ManageController.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Inquizition.Models;
+using Inquizition.Data;
 
 namespace Inquizition.Controllers
 {
     public class ManageController : Controller
     {
+        private readonly InquizitionContext _dbContext;
+        private readonly IFlashCardManager _flashCardManager;
+        public ManageController(InquizitionContext dbcontext, IFlashCardManager flashCardManager)
+        {
+            _dbContext = dbcontext;
+            _flashCardManager = flashCardManager;
+        }
+
         public IActionResult Index()
         {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
     }
