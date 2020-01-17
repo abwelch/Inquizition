8a8d5d175d4f14958c73e8980e4c8be83460649d
abwelch <abw28@zips.uakron.edu>
2020-01-16 21:46:07 -0600

    Implement get index

--- Controllers/ManageController.cs
+++ Controllers/ManageController.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Inquizition.Models;
 using Inquizition.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Inquizition.Controllers
 {
@@ -12,18 +11,41 @@ namespace Inquizition.Controllers
     {
         private readonly InquizitionContext _dbContext;
         private readonly IFlashCardManager _flashCardManager;
-        public ManageController(InquizitionContext dbcontext, IFlashCardManager flashCardManager)
+        private readonly IColorThemeManager _colorThemeManager;
+        private readonly IUserInfoManager _userInfoManager;
+
+        public ManageController(InquizitionContext dbcontext,
+            IFlashCardManager flashCardManager,
+            IColorThemeManager colorThemeManager,
+            IUserInfoManager userInfoManager)
         {
             _dbContext = dbcontext;
             _flashCardManager = flashCardManager;
+            _colorThemeManager = colorThemeManager;
+            _userInfoManager = userInfoManager;
         }
 
         public IActionResult Index()
         {
+            // Unathenticated users cannot access this page
             if (!User.Identity.IsAuthenticated)
             {
                 return RedirectToAction("Index", "Home");
             }
+            // Retrieve user info
+            UserInfo user = _userInfoManager.RetrieveUserInfo(User.Identity.Name);
+            ManageIndex viewModel = new ManageIndex
+            {
+                FlashCardInquizitorNames = _flashCardManager.RetrieveSetsAssociatedWithUser(user.Username)
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(ManageIndex selection)
+        {
+
             return View();
         }
     }
