2c5019a4e8d7be66d38e8a61a4be484b2563b2d4
abwelch <abw28@zips.uakron.edu>
2020-01-16 20:08:06 -0600

    Remove publish table and alter colortheme

--- Migrations/20200116234444_AlterColorTheme.Designer.cs
+++ Migrations/20200116234444_AlterColorTheme.Designer.cs
@@ -4,14 +4,16 @@ using Inquizition.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Inquizition.Migrations
 {
     [DbContext(typeof(InquizitionContext))]
-    partial class InquizitionContextModelSnapshot : ModelSnapshot
+    [Migration("20200116234444_AlterColorTheme")]
+    partial class AlterColorTheme
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20200116234444_AlterColorTheme.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Inquizition.Migrations
+{
+    public partial class AlterColorTheme : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Publish");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Creator",
+                table: "ColorTheme",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Creator",
+                table: "ColorTheme");
+
+            migrationBuilder.CreateTable(
+                name: "Publish",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InquizitorName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Published = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publish", x => x.ID);
+                });
+        }
+    }
+}
