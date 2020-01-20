d86c0deac716cbae32f95b06a723f8bdc5850711
abwelch <abw28@zips.uakron.edu>
2020-01-20 13:25:07 -0600

    Expand on manage model for public display

---
+++ Models/PublicDisplay.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Inquizition.Models
+{
+    public class PublicDisplay : ManageDisplay
+    {
+        public string Creator { get; set; }
+
+        public string Title { get; set; }
+    }
+}
