f4bd1c365ed98732e5f97afebbea3dad57a59e4d
abwelch <abw28@zips.uakron.edu>
2020-01-19 21:13:41 -0600

    Add policy controller and actions

---
+++ Controllers/PoliciesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Inquizition.Controllers
+{
+    public class PoliciesController : Controller
+    {
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult TermsOfUse()
+        {
+            return View();
+        }
+    }
+}
