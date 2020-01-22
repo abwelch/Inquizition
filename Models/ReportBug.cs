1693984a191f1ea6a7b366a0cb49b0361600b8c0
abwelch <abw28@zips.uakron.edu>
2020-01-22 11:33:59 -0600

    Add reportbug input model

---
+++ Models/ReportBug.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inquizition.Models
+{
+    public class ReportBug
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters.")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
+        public string Description { get; set; }
+
+        public string User { get; set; }
+    }
+}
